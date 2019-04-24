7b939e49a07dff58af11cf0511a37ea298d4d8e1
quanghung112 <laiquanghung112@gmail.com>
2019-04-24 12:05:12 +0700

    Thuc hanh toan tu

---
+++ Code.js
@@ -0,0 +1,7 @@
+inputWidht = prompt('nhap chieu cao =');
+inputHeight = prompt(' nhap chieu rong  = ');
+let Widht=parseInt(inputWidht);
+let Height=parseInt(inputHeight);
+let area=Widht*Height;
+document.write('The area = ' + area);
+
